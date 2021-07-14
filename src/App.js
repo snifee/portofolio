b78cb882678011d4951d826041419f63453ff4dd
snifee <50835842+snifee@users.noreply.github.com>
2021-07-14 08:48:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './Navbar'
 import Home from './Home';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
